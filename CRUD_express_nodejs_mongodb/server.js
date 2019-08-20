f2a2b70fac78255840a6b3e157402a93ab505455
Pedro Eugênio <pedroeugeniorlt@gmail.com>
2019-08-20 08:12:56 -0300

    first commit

---
+++ CRUD_express_nodejs_mongodb/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true}))
+
+app.listen(3000, function(){
+    console.log('server running on port 3000')  
+})
+
+app.set('view engine', 'ejs')
+
+app.get('/',(req,res) => {
+    res.render('index.ejs')
+})
+
+app.post('/show', (req,res) =>{
+    console.log(req.body)
+})
+
+
